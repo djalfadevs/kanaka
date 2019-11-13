2b81e830bad066df7878ac2240bf9c48f0561c95
lmml-l <43039968+lmml-l@users.noreply.github.com>
2019-11-13 18:26:12 +0100

    Cambios Totem

---
+++ Kanaka/Assets/Scripts/Totem/Totem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Totem : MonoBehaviour
+{
+    [SerializeField] private float hp;
+    [SerializeField] private float aliveTime = 3;
+    private float currentAliveTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAliveTime = aliveTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LifeCycle();
+    }
+
+    void LifeCycle()
+    {
+        currentAliveTime -= Time.deltaTime;
+        if (currentAliveTime <= 0)
+        {
+            Dead();
+        }
+    }
+
+    void Dead()
+    {
+        //Destroy(this.gameObject);
+    }
+}
