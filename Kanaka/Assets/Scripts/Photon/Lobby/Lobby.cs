e5876cee56cbea25efb611f5f8c73ccca90c1e9e
lmml-l <43039968+lmml-l@users.noreply.github.com>
2019-11-16 10:46:31 +0100

    Prueba lobby Photon
    Se ha creado una escena para probar el lobby planteado por photon

---
+++ Kanaka/Assets/Scripts/Photon/Lobby/Lobby.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class Lobby : MonoBehaviourPunCallbacks
+{
+    public Button connectBtn;
+    public Button joinRandomRoomBtn;
+    public Text Log;
+
+    public byte maxPlayersInRoom = 4;
+    public byte minPlayersInRoom = 2;
+
+    public int playerCounter;
+    public Text PlayerCounter;
+
+    public void Connect()
+    {
+        Debug.Log("Do something");
+        if (!PhotonNetwork.IsConnected)
+        {
+            Debug.Log("Do something");
+            if (PhotonNetwork.ConnectUsingSettings())
+            {
+                Log.text += "\nEstamos conectados al servidor";
+            }
+            else
+            {
+                Log.text += "\nError al conectar al servidor";
+            }
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Ahora estamos conectados al servidor de la region: " +
+            PhotonNetwork.CloudRegion);
+        connectBtn.interactable = false;
+        joinRandomRoomBtn.interactable = true;
+    }
+
+    public void JoinRandomRoom()
+    {
+        if(!PhotonNetwork.JoinRandomRoom())
+        {
+            Log.text += "\nFallo al unirse a la sala";
+        }
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Log.text += "\n No existen salas a las que unirse, creando una nueva";
+
+        if(PhotonNetwork.CreateRoom(null, new Photon.Realtime.RoomOptions() {MaxPlayers = maxPlayersInRoom }))
+        {
+            Log.text += "\n Sala creada con exito";
+        }
+        else
+        {
+            Log.text += "\n fallo al crear la sala";
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Log.text += "\n Unido a la sala";
+        joinRandomRoomBtn.interactable = false;
+    }
+
+    public void FixedUpdate()
+    {
+        if(PhotonNetwork.CurrentRoom != null)
+        {
+            playerCounter = PhotonNetwork.CurrentRoom.PlayerCount;
+        }
+        PlayerCounter.text = playerCounter + "/" + maxPlayersInRoom;
+    }
+}
