84b53edb9b21b45db3c9d4f4e1eb8eb3ee82321c
Alvaro Garcia <digifireuni@gmail.com>
2019-12-05 19:44:45 +0100

    Más menus actualizados

--- Kanaka/Assets/Scripts/CheckHeros.cs
+++ Kanaka/Assets/Scripts/CheckHeros.cs
@@ -6,16 +6,17 @@ using UnityEngine.UI;
 
 public class CheckHeros : MonoBehaviour
 {
-    public GameObject hero1;
-    public GameObject hero2;
-    public GameObject hero3;
-    public GameObject hero4;
+    public Button hero1;
+    public Button hero2;
+    public Button hero3;
+    public Button hero4;
     private string path;
     private User u;
 
+
     void Awake()
     {
-        path = Application.dataPath + "/UsersData/User.json";
+        path = Application.streamingAssetsPath + "/UsersData/User.json";
     }
 
     // Start is called before the first frame update
@@ -28,23 +29,22 @@ public class CheckHeros : MonoBehaviour
         List<int> l = u.charactersID;
         if (l.Contains(0))
         {
-            hero1.GetComponent<Image>().color = new Color(1,1,1,1);
-            hero1.GetComponent<Button>().enabled = true;
+            hero1.interactable = true;
         }
         if (l.Contains(1))
         {
-            hero2.GetComponent<Image>().color = new Color(1, 1, 1, 1);
-            hero2.GetComponent<Button>().enabled = true;
+            hero2.interactable = true;
+
         }
         if (l.Contains(2))
         {
-            hero3.GetComponent<Image>().color = new Color(1, 1, 1, 1);
-            hero3.GetComponent<Button>().enabled = true;
+            hero2.interactable = true;
+
         }
         if (l.Contains(3))
         {
-            hero4.GetComponent<Image>().color = new Color(1, 1, 1, 1);
-            hero4.GetComponent<Button>().enabled = true;
+            hero2.interactable = true;
+
         }
     }
 }
